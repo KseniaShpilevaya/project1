e5d775ff2ebb9ff5d3a0ccaedcd14d42311478b2
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-10-14 00:26:52 +0200

    commit 12

--- code/index.php
+++ code/index.php
@@ -1,7 +1,3 @@
 <?php
-$num_languages = 4;
-$months = 11;
-$days = $months * 16;
-$days_per_language = $days / $num_languages;
-echo $months / $num_languages, '<br>', $days_per_language;
+echo 8 ** 2;
 
