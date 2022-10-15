96d168351050024f49eb539ad09b6564fe7f9691
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-10-15 20:10:41 +0200

    commit 14 point 1

--- code/index.php
+++ code/index.php
@@ -1,11 +1,8 @@
 <?php
-$my_num = 7;
-$answer = $my_num;
-$answer += 2;
-$answer *= 2;
-$answer -= 2;
-$answer /= 2;
-$answer -= $my_num;
-echo $answer;
+$a = 10;
+$b = 3;
+echo ($a % $b), '<br>';
+
+
 
 
