fc0211a43859207ade5dea608a0f0b3bed73f65a
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-10-24 23:36:15 +0200

    commit 15

--- code/index.php
+++ code/index.php
@@ -1,24 +1,11 @@
 <?php
-// первое задание
-$a = 3;
-$del = [];
-
-for ($i = 1; $i <= $a; $i++) {
-    if ($a % $i == 0)
-        array_push($del, $i);
+function printStringReturnNumber($s)
+{
+    $a = rand(0, 100);
+    echo $s, '<br>';
+    return $a;
 }
 
-print_r($del);
-echo '<br>';
-
-// второе задание
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-$sum = 0;
-$i = 0;
-
-while ($sum <= 10) {
-    $sum += $arr[$i];
-    $i++;
-}
-echo $i;
+$st = "Привет!";
+$my_num = printStringReturnNumber($st);
+echo $my_num;
