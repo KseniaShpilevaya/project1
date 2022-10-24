49a28c394ac4c8e4b0eb55b8263597f594c5407c
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-10-24 20:41:39 +0200

    commit 14 point 5

--- code/index.php
+++ code/index.php
@@ -1,14 +1,9 @@
 <?php
-echo round(sqrt(379), 0), '<br>';
-echo round(sqrt(379), 1), '<br>';
-echo round(sqrt(379), 2), '<br>';
-
-$sq = sqrt(587);
-$ce = ceil($sq);
-$fl = floor($sq);
-$arr = ['ceil' => $ce, 'floor' => $fl];
-print_r($arr);
-echo $arr['ceil'];
+$arr = [4, -2, 5, 19, -130, 0, 10];
+print_r ($arr);
+echo ('<br>');
+echo min($arr), '<br>';
+echo max($arr);
 
 
 
