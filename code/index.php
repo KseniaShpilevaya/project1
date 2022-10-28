b1f1133c407de275c85519d857848c7dcc744e7c
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-10-28 21:20:53 +0200

    commit 18

--- code/index.php
+++ code/index.php
@@ -1,76 +1,61 @@
 <?php
-$a = [];
-$n = 10;
-$s = '';
-for ($i = 1; $i <= $n; $i++) {
-    $s .= 'x';
-    $a[] = $s;
+
+function func1($a, $b) {
+    if ($a + $b > 10)
+        return "true";
+    else
+        return "false";
 }
-print_r($a);
+
+echo func1(5, 1), '<br>';
+echo func1(10, 1);
 
 echo '<br>';
 
-function arrayFill($x, $n)
+function func2($a, $b) {
+    if ($a == $b)
+        return "true";
+    else
+        return "false";
+}
+
+echo func2(5, 5), '<br>';
+echo func2(10, 1);
+
+echo '<br>';
+
+$test = 1;
+if ($test== 0) { 
+    echo 'верно', '<br>'; 
+}
+echo $test == 0 ? 'верно' : '';
+
+echo '<br>';
+
+function sumDigits($n) {
+    $arr = str_split($n);
+    $sum = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+        $sum += $arr[$i];
+    }
+    return $sum;
+}
+
+$age = 20;
+
+if ($age < 10 || $age > 99)
+    echo "Число не двухзначное.", '<br>';
+else if ($age >= 10 && $age <= 99)
 {
-    $arr = [];
-    for ($i = 0; $i < $n; $i++) {
-        $arr[$i] = $x;
-    }
-    return $arr;
+    echo sumDigits($age), '<br>';
+    if (sumDigits($age) <= 9)
+        echo "Cумма цифр однозначна.", '<br>';
+    else 
+        echo "Cумма цифр двузначна.";
 }
 
-$arr = arrayFill('h', 5);
-
-print_r($arr);
-
 echo '<br>';
 
-$array_2D = [[[1, 2, 3], [4, 5], [6]]];
-$sum = 0;
-for ($row = 0; $row < sizeof($array_2D); $row++) {
-    for ($subarr = 0; $subarr < sizeof($array_2D[$row]); $subarr++) 
-        for ($i = 0; $i < sizeof($array_2D[$row][$subarr]); $i++)
-            $sum += $array_2D[$row][$subarr][$i];
-}
-
-echo $sum;
-
-echo '<br>';
-
-$arr1 = [];
-for ($subarr = 0; $subarr < 3; $subarr++) 
-    for ($i = 0; $i < 3; $i++) {
-        if ($subarr == 0)
-            $arr1[$subarr][$i] = $subarr + $i + 1;
-        if ($subarr == 1)
-            $arr1[$subarr][$i] = $subarr + $i + 3;
-        if ($subarr == 2)
-            $arr1[$subarr][$i] = $subarr + $i + 5;
-    }
-print_r($arr1);
-
-echo '<br>';
-
-$arr3 = [2, 5, 3, 9];
-$result = $arr3[0] * $arr3[1] + $arr3[2] * $arr3[3];
-echo $result;
-
-echo '<br>';
-
-$user = ['name' => 'Ksenia', 'surname' => 'Shpilevaya', 'patronymic' => 'Dmitrievna'];
-echo $user['name'] . " " . $user['surname'] . " " . $user['patronymic'];
-
-echo '<br>';
-
-$date = ['year' => 2022, 'month' => 'October', 'day' => 28];
-echo $date['year'] . "-" . $date['month'] . "-" . $date['day'];
-
-echo '<br>';
-
-$arr4 = ['a', 'b', 'c', 'd', 'e'];
-echo count($arr4), '<br>';
-
-echo $arr4[sizeof($arr4) - 1], '<br>';
-echo $arr4[sizeof($arr4) - 2];
-
-echo '<br>';
+$arr = [1, 2, 3];
+if (count($arr) == 3)
+    echo array_sum($arr);
