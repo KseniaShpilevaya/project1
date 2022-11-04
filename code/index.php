3272d86b812e35cc0ba6a620fe90edbdb59fc0a0
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-11-04 23:50:34 +0200

    1b

--- code/index.php
+++ code/index.php
@@ -3,5 +3,18 @@
 $str = 'ahb acb aeb aeeb adcb axeb';
 $regexp = '/a..b/'; 
 $matches = []; 
-$count = preg_match_all($regexp, $str, $matches);
+preg_match_all($regexp, $str, $matches);
 print_r($matches);
+
+echo '<br>';
+
+function cube($matches)
+	{
+		$result = pow($matches[0], 3); 
+		return $result;
+	}
+
+$str1 =  'a1b2c3';
+$regexp1 = '/[0-9]/'; 
+$str1 = preg_replace_callback($regexp1, 'cube', $str1);
+print_r($str1);
