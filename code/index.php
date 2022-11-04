0305e2f687e942bef387b7bbc39a06a2cfea662e
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-11-04 20:46:40 +0200

    1a

--- code/index.php
+++ code/index.php
@@ -1,30 +1,7 @@
 <?php
 
-$arr = [1, 2, 3, 4, 5];
-$center = array_sum($arr) / count($arr);
-
-echo $center;
-
-echo '<br>';
-
-echo array_sum(range(1, 100));
-
-echo '<br>';
-
-$sqrtNum = array_map('sqrt', $arr);
-print_r($sqrtNum);
-
-echo '<br>';
-
-$key = range('a', 'z');
-$value = range(1, 26);
-$arr1 = array_combine($key, $value);
-print_r($arr1);
-
-echo '<br>';
-
-$s = '1234567890';
-$arr2 = str_split($s); 
-$chunk = array_chunk($arr2, 2); 
-$chunkUn = array_map('implode', $chunk); 
-echo array_sum($chunkUn);
+$str = 'ahb acb aeb aeeb adcb axeb';
+$regexp = '/a..b/'; 
+$matches = []; 
+$count = preg_match_all($regexp, $str, $matches);
+print_r($matches);
