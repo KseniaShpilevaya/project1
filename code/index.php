7696960652919266c0fd6f41bb073b3553c3ca83
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2022-11-06 00:34:09 +0200

    3

--- code/index.php
+++ code/index.php
@@ -1,34 +1,82 @@
-<?php
-session_start();
-if(!empty($_POST)) { 
-    $_SESSION['useruser'] = ['surname' => $_POST['surname'],
-    'name' => $_POST['name'],
-    'age' => $_POST['age'],
-    'salary' => $_POST['salary']];
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hello</title>
-    </head>
-    <body>
-        <form action="index.php" method="POST">
-            <label for="surname">Фамилия:</label>
-            <input name="surname" required>
-            <label for="name">Имя:</label>
-            <input name="name" required>
-            <label for="age">Возраст:</label>
-            <input name="age" type="number" required>
-            <label for="salary">Зарплата:</label>
-            <input name="salary" type="number" required>
-            <input type="submit">
-        </form>
-        <form action="save.php"> 
-            <input type="submit" value="Посмотреть список">
-        </form>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avito</title>
+</head>
+<body>
+<!-- была форма с полями (email, выбор категории(название выше созданных папок), заголовок объявления и текст объявления, кнопка Добавить). -->
+    <div id="form">
+        <form action="save.php" method="post">
+            <label for="email">Email</label>
+            <input type="email" name="email" required>
+            
+            <label for="category">Category</label>
+            <select name="category" required>
+            <?php
+                $folders = "categories";
+                $categories = scandir($folders);
+                foreach($categories as $category) {
+                    if (($category != '.') && ($category != "..")) {
+                        echo "<option value=".$category.'>'.$category."</option>";
+                    }
+                }
+                ?>
+            </select>
+            
+            <label for="title">Title</label>
+            <input type="text" name="title" required>
+
+            <label for="description">Description</label>
+            <textarea rows="3" name="description"></textarea>
+
+            <input type="submit" value="Save">
+        </form> 
+    </div>
+    <div id="table">
+        <table>
+            <thead>
+                <th>Category</th>
+                <th>Email</th>
+                <th>Title</th>
+                <th>Description</th>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($categories as $category) {
+                        if (($category === '.') || ($category === '..')) {
+                            continue;
+                        }
+                        $categoryPath = "categories/{$category}";
+                        $emailsInCategory = scandir($categoryPath);
+                        foreach($emailsInCategory as $email) {
+                            if (($email === '.') || ($email === '..')) {
+                                continue;
+                            }
+                            $emailFolder = "categories/{$category}/{$email}";
+                            $advertisements = scandir($emailFolder);
+                            foreach($advertisements as $adv) {
+                                if (($adv === '.') || ($adv === '..')) {
+                                    continue;
+                                }
+                                $advPath = "categories/{$category}/{$email}/{$adv}";
+                                $advFile = fopen($advPath, "r");
+                                $descr = file_get_contents($advPath);
+                                fclose($advFile);
+                                echo "<tr>";
+                                echo "<td>{$category}</td>";
+                                echo "<td>{$email}</td>";
+                                echo "<td>".substr($adv, 0, -4)."</td>";
+                                echo "<td>{$descr}</td>";
+                                echo "</tr>";
+                            }
+                        }
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
 </html>
--- code/save.php
+++ code/save.php
@@ -1,9 +1,29 @@
 <?php
-session_start();
-echo "<ul>";
-	if (!empty($_SESSION['useruser'])) {
-		foreach($_SESSION['useruser'] as &$userData) {
-            echo "<li>".$userData."</li>";
-        }
-	}
-echo "</ul>";
+function redirectToHome(): void 
+{
+	header('Location: /');
+
+	exit();
+}
+
+if (false === isset($_POST['email'], $_POST['category'],  $_POST['title'], $_POST['description'])) {
+	redirectToHome();
+} // обработка, когда чего-то не хватает
+
+$email = $_POST['email'];
+$category = $_POST['category'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+
+$folderPath = "categories/{$category}/{$email}";
+
+if (false === file_exists($folderPath)) {
+    mkdir("categories/{$category}/{$email}", 0777);
+}
+
+$filePath = "categories/{$category}/{$email}/{$title}.txt";
+
+if (false === file_put_contents($filePath, $desc)) {
+	throw new Exceprion("Something went wrong");
+}
+redirectToHome();
